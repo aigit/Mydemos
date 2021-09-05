1ceb9963e8bf37ffb047baa92ae464c6b358af4d
liudelong <ldl_V_010823>
2021-09-06 00:06:31 +0800

    添加实例

--- src/main/java/org/study/MyApplication.java
+++ src/main/java/org/study/MyApplication.java
@@ -1,11 +1,26 @@
 package org.study;
 
 import lombok.extern.slf4j.Slf4j;
+import org.study.concurrent.TestInterrupt;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class MyApplication {
 
     public static void main(String[] args) {
-
+        TestInterrupt testInterrupt = new TestInterrupt();
+        testInterrupt.monitor();
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        testInterrupt.interupts();
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/org/study/concurrent/TestInterrupt.java
@@ -0,0 +1,37 @@
+package org.study.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class TestInterrupt {
+
+    private static Thread thread;
+
+    public void monitor(){
+
+        thread = new Thread(()->{
+            while (true){
+                if(thread.isInterrupted()){
+                    log.info("处理后世");
+                    break;
+                }
+                try{
+                    log.info("监控任务");
+                    TimeUnit.SECONDS.sleep(2);
+                }catch (InterruptedException e){
+                    thread.interrupt();
+                }
+            }
+        },"monitor");
+
+        thread.start();
+
+    }
+
+    public void interupts(){
+        thread.interrupt();
+    }
+
+}

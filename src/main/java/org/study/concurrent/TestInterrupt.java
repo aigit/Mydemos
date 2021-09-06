dd6eecfa5f7a707295c655de509f96160e22fbfd
liudelong <ldl_V_010823>
2021-09-06 23:04:03 +0800

    添加实例

--- src/main/java/org/study/concurrent/TestInterrupt.java
+++ src/main/java/org/study/concurrent/TestInterrupt.java
@@ -3,6 +3,7 @@ package org.study.concurrent;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 @Slf4j
 public class TestInterrupt {
@@ -27,7 +28,7 @@ public class TestInterrupt {
         },"monitor");
 
         thread.start();
-
+        LockSupport.park();
     }
 
     public void interupts(){

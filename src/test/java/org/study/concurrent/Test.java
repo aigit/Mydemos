72c1d2f5b25ceafbce771b77edaa7f2dbb644721
agit <liudelong5@126.com>
2022-04-29 11:59:48 +0800

    添加redisson单元测试

--- src/test/java/org/study/concurrent/Test.java
+++ src/test/java/org/study/concurrent/Test.java
@@ -2,15 +2,19 @@ package org.study.concurrent;
 
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.study.MyApplication;
 import org.study.concurrent.locks.ReentrantLockTest;
 
 import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Slf4j
 @SpringBootTest(classes = MyApplication.class)
@@ -18,6 +22,9 @@ public class Test {
 
     @Resource(name = "redissonConnectionFactory")
     private RedissonConnectionFactory redissonConnectionFactory;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     @org.junit.jupiter.api.Test
     public void testReentrantLock(){
@@ -33,8 +40,24 @@ public class Test {
         connection.setBit(bitKey,1,true);
         connection.setBit(bitKey,2,false);
         connection.setBit(bitKey,3,true);
+        connection.setBit(bitKey,4,true);
+        connection.setBit(bitKey,5,true);
+        connection.setBit(bitKey,6,false);
 
         log.info("Bit count:{}",connection.bitCount(bitKey));
+        /*final BitFieldSubCommands.BitFieldGet bitFieldGet = BitFieldSubCommands.
+                BitFieldGet.create(BitFieldSubCommands.BitFieldType.INT_32, BitFieldSubCommands.Offset.offset(4));*/
+        BitFieldSubCommands.BitFieldGetBuilder fieldGetBuilder = BitFieldSubCommands.create().
+                get(BitFieldSubCommands.BitFieldType.unsigned(6));
+        final BitFieldSubCommands bitFieldSubCommands = fieldGetBuilder.valueAt(0l);
+        final List<Long> longs = connection.bitField(bitKey, bitFieldSubCommands);
+        log.info("Bit count:{}",longs.get(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testHyperLogLog(){
+        final RedisConnection connection = redissonConnectionFactory.getConnection();
+        connection.hyperLogLogCommands();
     }
 
 }
